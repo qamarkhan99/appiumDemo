8e0a7b78152aef1240736c6e4bb627bddea717e3
qamar.munir <qamar.munir@10pearls.com>
2022-10-09 01:18:36 +0500

    Lecture01&2

--- src/test/java/testcases/OpenApp.java
+++ src/test/java/testcases/OpenApp.java
@@ -11,12 +11,14 @@ import java.time.Duration;
 public class OpenApp extends TestBase {
 
     @Test
-    public void clickAppButton()  {
+    public void clickAppButton() throws InterruptedException {
         System.out.println("Entering into click app test");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.android.permissioncontroller:id/permission_deny_button")));
-        driver.findElement(By.id("com.android.permissioncontroller:id/permission_deny_button")).click();
-        System.out.println("deny clicked");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.Button[@resource-id='com.booking:id/identity_landing_social_button']")));
+        driver.findElement(By.xpath("//android.widget.Button[@resource-id='com.booking:id/identity_landing_social_button']")).click();
+        Thread.sleep(10000);
+        System.out.println("Create your account clicked");
+
     }
 }
